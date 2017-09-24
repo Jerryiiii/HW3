026d6dd485c7c072c95d79470a1740090bbf3686
Jerryiiii <31806359+Jerryiiii@users.noreply.github.com>
2017-09-23 22:06:05 -0700

    Create sketch2.js

---
+++ sketch2.js
@@ -0,0 +1,14 @@
+background (255);  
+
+var b = 0
+ 
+  while(b < 340){
+  
+  stroke(0);
+  noFill();
+  ellipse (100 + b, 100 , 20, 20)
+  stroke(255)
+  fill(255)
+    rect(89 + b, 89 , 21, 21)
+   b = b + 10
+  }
